76cc5242ae107560bb4efbe2e55208c2523b4deb
bithighlander <bithighlander@gmail.com>
2024-05-10 23:28:38 -0600

    debug build

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.optimization.minimize = false; // Disable minification
-        }
+    webpack: (config) => {
+        config.optimization.minimize = false; // Disable minification for all builds
         return config;
     }
+    // webpack: (config, { isServer }) => {
+    //     if (!isServer) {
+    //         config.optimization.minimize = false; // Disable minification
+    //     }
+    //     return config;
+    // }
 };
 
 export default nextConfig;
